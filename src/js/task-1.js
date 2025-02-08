ac212125aa6cf7cd18775c5b33e4274b2b983aad
Liia <1991safronova@gmail.com>
2025-02-08 19:00:10 +0100

    task-1

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,10 +1,10 @@
- // Якщо імейл і пароль користувача збігаються, зберігайте дані з форми при сабмите
+// Якщо імейл і пароль користувача збігаються, зберігайте дані з форми при сабмите
 // у локальне сховище і змінюй кнопку login на logout і роби поля введення
 // Недоступними зміни.
 
-import { USER_DATA } from "./constans";
-import { onLoginFormSubmit } from "./handlers";
-import { refs } from "./refs";
+import { USER_DATA } from './constans';
+import { onLoginFormSubmit } from './handlers';
+import { refs } from './refs';
 
 // При перезавантаженні сторінки, якщо користувач залогінений, ми маємо бачити logout-кнопку
 // та недоступні для зміни поля з даними користувача.
@@ -14,11 +14,6 @@ import { refs } from "./refs";
 // Якщо введені дані не збігаються з потрібними даними, викликати аlert і
 // повідомляти про помилку
 
-document.addEventListener('DOMContentLoaded',)initPage);
+document.addEventListener('DOMContentLoaded', initPage);
 
 refs.onLoginForm.addEventListener('submit', onLoginFormSubmit);
-
-
-
-
-
